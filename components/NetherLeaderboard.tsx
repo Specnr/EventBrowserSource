d49ecf6e5c3090f6ed0cc9fcea8d61dab805fd2f
Specnr <specnrr@gmail.com>
2025-01-23 15:56:49 -0500

    added other stats

--- components/NetherLeaderboard.tsx
+++ components/NetherLeaderboard.tsx
@@ -11,14 +11,18 @@ const fetcher = async (url: string) => {
   const data = (await fetch(url).then((res) => res.json())) as Result;
   const input = await getStatsInputFromResult(data)
   if (!input) {
-    return []
+    return {}
   }
 
   const stats = (await fetch(GET_INTERNAL_STATS_URL(input.whitelist, input.start))
     .then((res) => res.json())) as { stats: StatsResult, nickname: string }[];
 
-  stats.sort((a, b) => b.stats.nether.count - a.stats.nether.count)
-  return stats
+  return {
+    nether: stats.toSorted((a, b) => b.stats.nether.count - a.stats.nether.count).map(s => ({ nickname: s.nickname, stats: s.stats.nether })),
+    secondStructure: stats.toSorted((a, b) => b.stats.second_structure.count - a.stats.second_structure.count).map(s => ({ nickname: s.nickname, stats: s.stats.second_structure })),
+    firstPortal: stats.toSorted((a, b) => b.stats.first_portal.count - a.stats.first_portal.count).map(s => ({ nickname: s.nickname, stats: s.stats.first_portal })),
+    stronghold: stats.toSorted((a, b) => b.stats.stronghold.count - a.stats.stronghold.count).map(s => ({ nickname: s.nickname, stats: s.stats.stronghold }))
+  }
 }
 
 interface Props {
@@ -36,32 +40,100 @@ export default function NetherLeaderboard({ eventId, rows, cols }: Props) {
     return <Spinner />
   }
 
-  if (!data || error) {
+  if (!data || error || !data.nether) {
     return <h1>Invalid event!</h1>
   }
 
   return (
-    <Leaderboard
-      rows={rows}
-      cols={cols}
-      generateCell={(i, j) => {
-        const playerIndex = (rows * i) + j
-        if (playerIndex >= data.length) {
-          return
-        }
+    <div className="space-y-4">
+      <Leaderboard
+        rows={rows}
+        cols={cols}
+        generateCell={(i, j) => {
+          const playerIndex = (rows * i) + j
+          if (playerIndex >= data.nether.length) {
+            return
+          }
 
-        return (
-          <tr key={j}>
-            <td className="p-0">
-              <PlayerEntry
-                place={playerIndex + 1}
-                name={data[playerIndex].nickname}
-                scoreText={getScoreTextFromStat(data[playerIndex].stats.nether)}
-              />
-            </td>
-          </tr>
-        )
-      }}
-    />
+          return (
+            <tr key={j}>
+              <td className="p-0">
+                <PlayerEntry
+                  place={playerIndex + 1}
+                  name={data.nether[playerIndex].nickname}
+                  scoreText={getScoreTextFromStat(data.nether[playerIndex].stats)}
+                />
+              </td>
+            </tr>
+          )
+        }}
+      />
+      <Leaderboard
+        rows={rows}
+        cols={cols}
+        generateCell={(i, j) => {
+          const playerIndex = (rows * i) + j
+          if (playerIndex >= data.secondStructure.length) {
+            return
+          }
+
+          return (
+            <tr key={j}>
+              <td className="p-0">
+                <PlayerEntry
+                  place={playerIndex + 1}
+                  name={data.secondStructure[playerIndex].nickname}
+                  scoreText={getScoreTextFromStat(data.secondStructure[playerIndex].stats)}
+                />
+              </td>
+            </tr>
+          )
+        }}
+      />
+      <Leaderboard
+        rows={rows}
+        cols={cols}
+        generateCell={(i, j) => {
+          const playerIndex = (rows * i) + j
+          if (playerIndex >= data.firstPortal.length) {
+            return
+          }
+
+          return (
+            <tr key={j}>
+              <td className="p-0">
+                <PlayerEntry
+                  place={playerIndex + 1}
+                  name={data.firstPortal[playerIndex].nickname}
+                  scoreText={getScoreTextFromStat(data.firstPortal[playerIndex].stats)}
+                />
+              </td>
+            </tr>
+          )
+        }}
+      />
+      <Leaderboard
+        rows={rows}
+        cols={cols}
+        generateCell={(i, j) => {
+          const playerIndex = (rows * i) + j
+          if (playerIndex >= data.stronghold.length) {
+            return
+          }
+
+          return (
+            <tr key={j}>
+              <td className="p-0">
+                <PlayerEntry
+                  place={playerIndex + 1}
+                  name={data.stronghold[playerIndex].nickname}
+                  scoreText={getScoreTextFromStat(data.stronghold[playerIndex].stats)}
+                />
+              </td>
+            </tr>
+          )
+        }}
+      />
+    </div>
   )
 }
