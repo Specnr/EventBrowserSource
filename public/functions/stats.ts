6d8e273adaf101335b81a030a5fc5f7e73a66d37
Specnr <specnrr@gmail.com>
2025-01-25 13:03:35 -0500

    always use first start

--- public/functions/stats.ts
+++ public/functions/stats.ts
@@ -4,11 +4,6 @@ import { uuidToIGN } from "./player";
 
 export const getStatsInputFromResult = async (result: Result) => {
   let start = result.event.starts[0]
-  result.event.starts.forEach(s => {
-    if ((s * 1000) < Date.now()) {
-      start = s
-    }
-  })
 
   const whitelist = await Promise.all(
     result.event.whitelist.map(async p => await uuidToIGN(p))
